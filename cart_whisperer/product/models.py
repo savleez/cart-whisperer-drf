1d068480dee12a52b649b79d0da9b47103b1bf00
Sergio Velez <svleez@gmail.com>
2024-03-24 14:03:42 -0500

    feat: :building_construction: Add product models
    Add Product, Category and Brand models

--- cart_whisperer/product/models.py
+++ cart_whisperer/product/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Nombre")
+    parent = models.ForeignKey(
+        to="self", on_delete=models.PROTECT, null=True, verbose_name="Categoría padre"
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Categoría"
+        verbose_name_plural = "Categorías"
+        ordering = ["name"]
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Nombre")
+
+    class Meta:
+        verbose_name = "Marca"
+        verbose_name_plural = "Marcas"
+        ordering = ["name"]
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Nombre")
+    description = models.TextField(blank=True, null=True, verbose_name="Descripción")
+    brand = models.ForeignKey(to=Brand, on_delete=models.CASCADE, verbose_name="Marca")
+    category = models.ForeignKey(
+        to=Category, on_delete=models.SET_NULL, null=True, verbose_name="Categoría"
+    )
+
+    class Meta:
+        verbose_name = "Producto"
+        verbose_name_plural = "Productos"
+        ordering = ["name"]
